92d7f0f4a89234247bc8cca60bd89c5261284902
Ashish <ashishizofficial@gmail.com>
2021-06-20 23:40:37 +0530

    initial init

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ function sync(){
     let selectInputValue = document.getElementById("searchBox").value;
     console.log(selectInputValue);
     let xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://192.168.187.130/cgi-bin/server.py?var="+selectInputValue, false);
+    xhr.open("GET", "http://192.168.187.130/cgi-bin/server.py?var="+selectInputValue, true);
     xhr.send();
-    let output = xhr.responseText;
-    document.getElementById("container").innerHTML = output;
+    xhr.onload = function (){
+      let output = xhr.responseText;
+      document.getElementById("container").innerHTML = output;
+    }
   }   
   
 let i = 0;
